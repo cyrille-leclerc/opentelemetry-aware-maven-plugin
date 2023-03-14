6027f36c9c7f3f7b74d2be60d87dba0917361669
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2023-03-14 16:58:51 +0100

    Better example

--- src/main/java/com/example/maven/TestMojo.java
+++ src/main/java/com/example/maven/TestMojo.java
@@ -1,7 +1,9 @@
 package com.example.maven;
 
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.ContextStorage;
 import io.opentelemetry.context.Scope;
@@ -31,6 +33,18 @@ public class TestMojo extends AbstractMojo {
                     " loaded " +
                     " by: " + contextStorageClass.getClassLoader() +
                     " from: " + Optional.of(contextStorageClass.getProtectionDomain().getCodeSource()).map(source -> source.getLocation().toString()).orElse("#unknown#"));
+
+            Tracer tracer = GlobalOpenTelemetry.get().getTracer("com.example.maven");
+            Span childSpan = tracer.spanBuilder("otel-aware-goal-subspan").setAttribute("an-attribute", "a-value").startSpan();
+            try (Scope ignored2 = childSpan.makeCurrent()) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                childSpan.end();
+            }
         }
     }
 }
